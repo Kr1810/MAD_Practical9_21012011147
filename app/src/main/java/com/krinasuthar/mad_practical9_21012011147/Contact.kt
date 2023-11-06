6f3797abbae69e5de61e3bec2075305266ba174e
Krina Suthar <98055905+Kr1810@users.noreply.github.com>
2023-11-06 20:26:50 +0530

    Contact.kt

---
+++ app/src/main/java/com/krinasuthar/mad_practical9_21012011147/Contact.kt
@@ -0,0 +1,8 @@
+package com.example.mad_practical11_21012011147
+class Contact (var id:String, var lat:String, var long:String, var name:String, var phone:String,var address:String) {
+
+    companion object {
+
+        var contactArray: List<Contact> = ArrayList()
+    }
+}
